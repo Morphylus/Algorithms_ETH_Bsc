d4a330367d1c1cbda69227d315912834772b56f5
marti <marti@LAPTOP-KU1Q6MTR.zyxel.com>
2021-01-01 15:58:24 +0100

    Update 1.1.5
    - Added notes to QuickSorting algorithm

--- Algorithms_ETH_Bsc/Sorting_and_Searching/QuickSort.java
+++ Algorithms_ETH_Bsc/Sorting_and_Searching/QuickSort.java
@@ -1,7 +1,10 @@
 
 public class QuickSort {
 	public static int[] quickSort(int[] A, int left, int right) {
+		/* Recursive calls of quickSort on the subproblems*/
 		if (left < right) {
+			/*k will be in the right position, so we continue to do quickSearch on all the other elements
+			 * that are not in the right position*/
 			int k = partition(A, left, right);
 			quickSort(A, left, k - 1);
 			quickSort(A, k + 1, right);
@@ -11,19 +14,28 @@ public class QuickSort {
 	}
 	
 	public static int partition(int[] A, int left, int right) {
+		/* First, we set a leftpointer, a rightpinter and a pivot element (the one all to the right)*/
 		int leftPointer = left;
 		int rightPointer = right - 1;
 		int pivot = A[right];
 		
+		/* First while loop: loops until the element at 'leftPointer' is bigger then the pivot
+		 * Second while loop: loops until the element at 'rightPointer' is smaller then the pivot
+		 * Now those two elements get changed
+		 * This continues until the leftPointer and rightPointer intersect, now the pivot element gets
+		 * inserted in the middle of it, it is now in the right place*/
 		do {
+			// Move leftPointer
 			while (leftPointer < right && A[leftPointer] <= pivot) {
 				leftPointer++;
 			}
 			
+			// Move rightPointer
 			while (rightPointer > left && A[rightPointer] >= pivot) {
 				rightPointer--;
 			}
 			
+			// Change position of elements
 			if (leftPointer < rightPointer) {
 				int temp = A[leftPointer];
 				A[leftPointer] = A[rightPointer];
@@ -31,6 +43,7 @@ public class QuickSort {
 			}
 		} while (leftPointer < rightPointer);
 		
+		// Put pivot in the middle (correct position)
 		int temp = A[leftPointer];
 		A[leftPointer] = A[right];
 		A[right] = temp;
