01afdb105bf5cfd722f06a7edaab0540a2f066a7
marti <marti@LAPTOP-KU1Q6MTR.zyxel.com>
2021-01-01 14:46:03 +0100

    Update 1.1.3
    - Added MergeSort

--- DynamicProgrammingPractice/Sorting_and_Searching/PlayGround.java
+++ DynamicProgrammingPractice/Sorting_and_Searching/PlayGround.java
@@ -6,8 +6,8 @@ public class PlayGround {
 	public static void main(String[] args) {
 		Random rand = new Random();
 		
-		int[] A = ArrayCreation.createRandomArray(10, 30);
-		System.out.println(Arrays.toString(InsertionSort.insertionSort(A)));
+		int[] A = ArrayCreation.createRandomArray(8, 30);
+		System.out.println(Arrays.toString(MergeSort.mergeSort(A, 0, A.length-1)));
 		
 		
 	}
