347eafa4765963b7cf726027deffdb764634e25c
Murphylus <murphylus.pign@gmail.com>
2021-01-03 13:35:13 +0100

    Update 1.3.1
    - Added comments for HeapSort

--- Algorithms_ETH_Bsc/Sorting_and_Searching/HeapSort.java
+++ Algorithms_ETH_Bsc/Sorting_and_Searching/HeapSort.java
@@ -3,10 +3,12 @@ public class HeapSort {
 	public static int[] heapSort(int[] A) {
 		int mid = (A.length-1)/2;
 		
+		/* This for loop helps us create the heap in order to then heapsort the array*/
 		for (int i = mid; i >= 0; i--) {
 			restoreHeapCondition(A, i, A.length-1);
 		}
 		
+		/* After the heap condition is fullfilled, we can begin sorting*/
 		for (int i = A.length - 1; i > 0; i--) {
 			int temp = A[0];
 			A[0] = A[i];
