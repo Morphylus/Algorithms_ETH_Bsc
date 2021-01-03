37fd45afcb3e0111aefe6e5c9630b0d75cc4406a
Murphylus <murphylus.pign@gmail.com>
2021-01-03 15:43:11 +0100

    Update 1.3.2
    - Finished commenting HeapSort

--- Algorithms_ETH_Bsc/Sorting_and_Searching/HeapSort.java
+++ Algorithms_ETH_Bsc/Sorting_and_Searching/HeapSort.java
@@ -10,10 +10,13 @@ public class HeapSort {
 		
 		/* After the heap condition is fullfilled, we can begin sorting*/
 		for (int i = A.length - 1; i > 0; i--) {
+			
+			/* Takes the max and puts it at the end of the array*/
 			int temp = A[0];
 			A[0] = A[i];
 			A[i] = temp;
 			
+			/* Continues on subarray that is not sorted*/
 			restoreHeapCondition(A, 0, i-1);
 		}
 		
@@ -22,16 +25,20 @@ public class HeapSort {
 	
 	
 	public static void restoreHeapCondition(int[] A, int mainPointer, int arrayLength) {
+		/* This while loop continues as long as the "node" has children*/
 		while (2 * mainPointer <= arrayLength) {
 			int childrenPointer = 2 * mainPointer;
 			
 			if (childrenPointer + 1 <= arrayLength) {
+				/* Change pointer to the bigger child*/
 				if (A[childrenPointer] < A[childrenPointer + 1]) {
 					childrenPointer++;
 				}
+				/* If the parent node is bigger than the children, we can stop this iteration*/
 				if (A[mainPointer] >= A[childrenPointer]) {
 					break;
 				} else {
+					/* If child is bigger than parent node, change them */
 					int temp = A[mainPointer];
 					A[mainPointer] = A[childrenPointer];
 					A[childrenPointer] = temp;				
@@ -40,6 +47,7 @@ public class HeapSort {
 			
 			mainPointer = childrenPointer;
 			
+			/* Stop if we are through the whole array*/
 			if (arrayLength == 0) {
 				break;
 			}
