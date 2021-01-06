3dc02b2605f38465418192457e62a1e22263894d
marti <marti@192.168.1.53>
2021-01-06 09:29:49 +0100

    Update 1.3.3
    - Corrected HeapSort Algorithm

--- Algorithms_ETH_Bsc/Sorting_and_Searching/HeapSort.java
+++ Algorithms_ETH_Bsc/Sorting_and_Searching/HeapSort.java
@@ -3,10 +3,8 @@ public class HeapSort {
 	public static int[] heapSort(int[] A) {
 		int mid = (A.length-1)/2;
 		
-<<<<<<< HEAD
-=======
 		/* This for loop helps us create the heap in order to then heapsort the array*/
->>>>>>> branch 'main' of https://github.com/Morphylus/Algorithms.git
+
 		for (int i = mid; i >= 0; i--) {
 			restoreHeapCondition(A, i, A.length-1);
 		}
@@ -30,33 +28,30 @@ public class HeapSort {
 	public static void restoreHeapCondition(int[] A, int mainPointer, int arrayLength) {
 		/* This while loop continues as long as the "node" has children*/
 		while (2 * mainPointer <= arrayLength) {
+			
+			/* Set a pointer to the left child*/
 			int childrenPointer = 2 * mainPointer;
 			
+			/* If there is a right child that is bigger than the left child, ignore left child
+			 * and continue with right child*/
 			if (childrenPointer + 1 <= arrayLength) {
-<<<<<<< HEAD
-=======
-				/* Change pointer to the bigger child*/
->>>>>>> branch 'main' of https://github.com/Morphylus/Algorithms.git
 				if (A[childrenPointer] < A[childrenPointer + 1]) {
 					childrenPointer++;
 				}
-				/* If the parent node is bigger than the children, we can stop this iteration*/
-				if (A[mainPointer] >= A[childrenPointer]) {
-					break;
-				} else {
-					/* If child is bigger than parent node, change them */
-					int temp = A[mainPointer];
-					A[mainPointer] = A[childrenPointer];
-					A[childrenPointer] = temp;				
-				}
 			}
 			
-			mainPointer = childrenPointer;
-			
-			/* Stop if we are through the whole array*/
-			if (arrayLength == 0) {
+			/* HeapCondition fullfiled, we dont need to check further*/
+			if (A[mainPointer] >= A[childrenPointer]) {
 				break;
 			}
+			
+			/* Change largest child with parent node*/
+			int temp = A[mainPointer];
+			A[mainPointer] = A[childrenPointer];
+			A[childrenPointer] = temp;
+			
+			/* Continue with child*/
+			mainPointer = childrenPointer;
 		}
 		
 	}
