857fa79f27a071c0a930cf1389887125cb400910
marti <marti@192.168.1.53>
2021-01-09 17:32:26 +0100

    Topological Sorting

--- Algorithms_ETH_Bsc/Graph_Algorithms/TopoSort.java
+++ Algorithms_ETH_Bsc/Graph_Algorithms/TopoSort.java
@@ -17,12 +17,14 @@ public class TopoSort {
 	public void topoSort() {
 		Stack<Integer> S = new Stack<Integer>();
 
+		/* Stores the inDegrees of each vertex*/
 		for (int i = 0; i < g.V; i++) {
 			for (int r = 0; r < g.L.get(i).size(); r++) {
 				inDeg[g.L.get(i).get(r).dest]++;
 			}
 		}
-
+		
+		/* All the vertecies that do not have a predecessor are added to the stack*/
 		for (int i = 0; i < g.V; i++) {
 			if (inDeg[i] == 0) {
 				S.push(i);
@@ -31,19 +33,30 @@ public class TopoSort {
 
 		int i = 0;
 
+		/* The while loop continues until every vertex has been checked*/
 		while (!S.isEmpty()) {
+			/* Takes out a vertex that has no predecessor, puts its index in the topoOrder array at position 'i',
+			 * then 'i' gets incremented, where the next vertex will be put in*/
 			int v = S.pop();
 			topoOrder[v] = i;
 			i++;
 
+			/* Each successor of the vertex that was taken out has now one predecessor less, so we
+			 * decrement the in degree of each successor of 'v'*/
 			for (int k = 0; k < g.L.get(v).size(); k++) {
 				inDeg[g.L.get(v).get(k).dest]--;
 				if (inDeg[g.L.get(v).get(k).dest] == 0) {
+					/* If a vertex has now in degree = 0, it will be added to our Stack, since that one will
+					 * come next in the topological order*/
 					S.push(g.L.get(v).get(k).dest);
 				}
 			}
 		}
 
+		/* If the amount of vertices in our topologically ordered array is equals to the amount of nodes, the whole
+		 * graph has been added successfully to the topological sort and we can print it out
+		 * If the amount does not correspond to the amount of vertices, topoSort was unsuccessful(even though it
+		 * scanned all the vertices) and the conclusion is that we have a cycle*/
 		if (i == g.V) {
 			print(topoOrder);
 		} else {
